2bb754f556f6247ca2e423634cea69ec8e6354fb
Maram Raphael Samson <maramraphaelsamson@gmail.com>
2025-12-11 02:31:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5584923426-f914a",
+  "appId": "1:797468647992:web:05f6e5075a45541664bccd",
+  "apiKey": "AIzaSyAruLQhPZFdrrGgUYsla51C_8NeV7U5TQk",
+  "authDomain": "studio-5584923426-f914a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "797468647992"
+};
